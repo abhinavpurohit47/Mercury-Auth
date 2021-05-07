215a0ed126ee0c7859e9c4e7933000b4dbfe82c9
Abhinav <purohitabhinav01@gmail.com>
2021-05-07 18:12:22 +0530

    added CORS

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -7,7 +7,7 @@ import { GET_ERRORS, SET_CURRENT_USER, USER_LOADING } from "./types";
 // Register User
 export const registerUser = (userData, history) => dispatch => {
   axios
-    .post("https://themercury.herokuapp.com/api/users/register", userData)
+    .post("http://themercury.herokuapp.com/api/users/register", userData)
     .then(res => history.push("/login"))
     .catch(err =>{
     console.log(err)
@@ -22,7 +22,7 @@ export const registerUser = (userData, history) => dispatch => {
 // Login - get user token
 export const loginUser = userData => dispatch => {
   axios
-    .post("https://themercury.herokuapp.com/api/users/login", userData)
+    .post("http://themercury.herokuapp.com/api/users/login", userData)
     .then(res => {
       // Save to localStorage
 
